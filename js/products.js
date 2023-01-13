cdd669bef068ec6542eacbf44535451669f1ff92
helmiazkia <azkiasekian@gmail.com>
2023-01-13 12:49:06 +0700

    first commit

---
+++ js/products.js
@@ -0,0 +1,13 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Celana",
+    img : "celana.png",
+    price : 4500000
+  },
+  124: {
+    name : "Sepatu",
+    img : "sepatu.png",
+    price : 3400000
+  }
+};
